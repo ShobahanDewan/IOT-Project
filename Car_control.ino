114e5656936ad37c76ab3c95c65a11d569b366b6
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-05-19 23:55:18 +0600

    Add files via upload

---
+++ Car_control.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+Servo myServo;
+
+const int servoPin = 9;
+char bluetoothData;
+
+void setup() {
+  Serial.begin(9600);
+  myServo.attach(servoPin);
+  myServo.write(0); // Initial position
+}
+
+void loop() {
+  if (Serial.available()) {
+    bluetoothData = Serial.read();
+
+    switch (bluetoothData) {
+      case 'F':
+        // Move forward
+        break;
+      case 'D':
+        // Move backward
+        break;
+      case 'L':
+        // Turn left
+        break;
+      case 'R':
+        // Turn right
+        break;
+      case 'S':  // Servo to 90 degrees
+        myServo.write(90);
+        break;
+      case 'B':  // Servo back to 0 degrees
+        myServo.write(0);
+        break;
+    }
+  }
+}
